6b95d3aa644d71b8a876f7826ff7e95225aa0be6
Metaboom4304 <159423683+Metaboom4304@users.noreply.github.com>
2025-07-23 11:58:35 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// config.js
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   DEVELOPER_IDS: process.env.DEVELOPER_IDS
     .split(',')
     .map(id => parseInt(id.trim(), 10)),
+  MAP_URL: process.env.MAP_URL,
 };
