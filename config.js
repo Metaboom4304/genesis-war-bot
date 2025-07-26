dfbad5c730d91db8d964e90b2b8ba0002a60d1d3
Metaboom4304 <159423683+Metaboom4304@users.noreply.github.com>
2025-07-26 10:37:03 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  // ...другие параметры
+  mapEnabled: true, // true — карта включена, false — отключена
+};
+module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   DEVELOPER_IDS: process.env.DEVELOPER_IDS
     .split(',')
