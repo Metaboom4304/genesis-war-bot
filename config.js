79e7adec71481d8ac4a18a8973c4e3f6f2f96347
Metaboom4304 <159423683+Metaboom4304@users.noreply.github.com>
2025-08-04 14:16:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN,
-  MAP_URL: process.env.MAP_URL,
-  DEVELOPER_IDS: process.env.DEVELOPER_IDS
-    ? process.env.DEVELOPER_IDS.split(',').map(id => Number(id.trim()))
-    : [],
-  mapEnabled: true
+  // Ваш токен бота
+  botToken: '7982838066:AAEoYlj-nFUs0mac0eUsiIKtslimQkIKQNo',
+  // Настройки для авторизации
+  auth: {
+    required: true,  // Требовать авторизацию
+    adminId: '766057421',  // Ваш ID для доступа
+  },
 };
+
 console.log('[CONFIG DEBUG] Developer IDs:', module.exports.DEVELOPER_IDS);
 console.log('[CONFIG DEBUG] Type of first ID:', typeof module.exports.DEVELOPER_IDS[0]);
