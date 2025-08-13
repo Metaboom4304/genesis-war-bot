45e5cda48a487dc8727db1243350daeede438af9
Metaboom4304 <159423683+Metaboom4304@users.noreply.github.com>
2025-08-13 21:20:50 +0500

    Update map.js

--- commands/map.js
+++ commands/map.js
@@ -2,6 +2,14 @@ export default {
   name: 'map',
   async execute(bot, msg) {
     const chatId = msg.chat.id;
-    await bot.sendMessage(chatId, '🗺 Карта работает!');
+
+    // Прямая ссылка на твою карту
+    const mapUrl = 'https://genesis-data.onrender.com';
+
+    await bot.sendMessage(
+      chatId,
+      `🗺 Открыть карту можно по ссылке:\n${mapUrl}`,
+      { disable_web_page_preview: false }
+    );
   }
 };
