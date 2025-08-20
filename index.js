289631c58a68f3f2cf7eb53547168c04fca57323
Metaboom4304 <159423683+Metaboom4304@users.noreply.github.com>
2025-08-21 00:42:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 // --- КОНЕЦ CORS ---
 
 // Middleware для парсинга JSON с увеличенным лимитом
-app.use(express.json({ limit: '50mb' })); // Увеличенный лимит
+app.use(express.json({ limit: '80mb' })); // Увеличенный лимит
 
 // --- Мидлвар для логирования запросов ---
 app.use((req, res, next) => {
